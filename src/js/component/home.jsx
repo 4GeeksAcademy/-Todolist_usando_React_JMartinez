ce7d6c7f39a3e9d18980dd5f994395b7ff32e47c
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-08-14 15:59:33 +0000

    Falta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,56 @@
-import React from "react";
 
+import React, { useState, useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
+
+
+	const Style = { width: "90%" }
+
+	const [array, setArray] = useState('');
+
+	const [valori, setValori] = useState('');
+
+	const [list, setList] = useState([1, 2, 3]);
+
+
+
+	const Submi = () => {
+
+		setList(list.concat(array));
+
+	};
+
+
+	useEffect(() => {
+		document.addEventListener('keydown', detectKeyDown, true)
+	}, [list])
+
+	const detectKeyDown = (e) => {
+		let valores = String(e.key)
+		console.log(valores)
+		if (valores = "Enter") {
+			setList()
+		}
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="text-center m-auto mt-5">
+			<input style={Style} value={array} onChange={(e) => setArray(e.target.value)} />
+
+			<ul>
+				{
+					list.map((item) => {
+						return (
+							<li key={item}>{item}</li>)
+					})}
+
+			</ul>
+		</div >
 	);
 };
 
