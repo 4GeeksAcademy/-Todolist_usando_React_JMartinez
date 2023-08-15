ecf52ef28a2f2bfc37cacd45cf85748dccbca508
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-08-15 15:24:59 +0000

    Falta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,11 +9,13 @@ const Home = () => {
 	const [array, setArray] = useState('');
 	const [list, setList] = useState([]);
 
+
 	const detectKeyDown = (event) => {
 
 		if (event.key === "Enter" && array !== "") {
-			setList(list.concat(array));
+			setList(list.concat({ label: array }));
 			setArray("")
+
 		}
 	}
 
@@ -32,6 +34,83 @@ const Home = () => {
 		setList(array2)
 
 	}
+
+	async function CRUS() {
+		try {
+			const respu = await
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/Jorge', {
+					method: "POST",
+					body: JSON.stringify([]),
+					headers: {
+						"Content-Type": "application/json"
+					}
+
+				})
+
+			const data = await respu.json()
+			console.log(data);
+
+
+		} catch (error) {
+
+			console.log(error);
+		}
+
+
+	}
+
+
+
+	async function obtenerInfo() {
+
+		try {
+			const res = await
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/Jorge', {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+
+					}
+
+				})
+
+			const data = await res.json()
+			setList(data)
+
+		} catch (error) {
+
+			console.log(error);
+		}
+	}
+
+	async function actualizar() {
+
+		try {
+			const resa = await
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/Jorge', {
+					method: "POST",
+					body: JSON.stringify(list),
+					headers: {
+						"Content-Type": "application/json"
+					},
+					const moto = await resa.json()
+
+				})
+
+		} catch (error) {
+
+			console.log(error);
+		}
+	}
+
+
+	useEffect(function () {
+		CRUS()
+		obtenerInfo()
+		actualizar()
+	}, [])
+
+
 	return (
 
 		<div className="m-auto mt-5 ">
@@ -44,10 +123,10 @@ const Home = () => {
 					{
 						list.map((item, index) => (
 							/* 	return ( */
-							<li className="fw-light list-group-item d-flex justify-content-between align-items-center list-group-item-action border border-0" style={Style} key={index}>{item}
+							<li className="fw-light list-group-item d-flex justify-content-between align-items-center list-group-item-action border border-0" style={Style} key={index}>{item.label}
 
 								<a className=" cruz badge text-danger" onClick={() => pepe(index)} id={index} key={index}>
-									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" momia bi bi-x-square" viewBox="0 0 16 16">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" momia bi bi-x-square" viewBox="0 0 16 16">
 										<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
 										<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
 									</svg>
@@ -62,8 +141,8 @@ const Home = () => {
 
 			</div>
 			<div className="monP m-auto" style={Style2}>
-				<div row className="m-auto border border-top-0" id="diva" > </div>
-				<div row className="m-auto border border-top-0" id="divb" > </div>
+				<div className="m-auto border border-top-0" id="diva" > </div>
+				<div className="m-auto border border-top-0" id="divb" > </div>
 			</div>
 		</div>
 
